1d1cffa9fd58e9f66b9dae599e1286ceb50046a7
Daniel <danielrs9504@gmail.com>
2022-08-03 20:25:07 -0500

    Agrega estilos al detalle del item

--- frontend/src/pages/ItemPage.tsx
+++ frontend/src/pages/ItemPage.tsx
@@ -1,12 +1,17 @@
-import { Breadcrumb, Col, Image, Layout, Row } from "antd";
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { ShopOutlined, ShoppingCartOutlined } from "@ant-design/icons";
+import { Breadcrumb, Button, Col, Layout, Row, Typography } from "antd";
 import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import LoadingSpin from "../components/LoadingSpin";
 import { useAppDispatch, useAppSelector } from "../redux/hooks";
 import { searchById } from "../redux/itemsSlice";
+import styles from "../styles/App.module.css";
 
 const ItemPage = () => {
   // * ========== Variables ==========
+  const { Title, Paragraph } = Typography;
   const { Content } = Layout;
   const dispatch = useAppDispatch();
   const { id } = useParams();
@@ -24,33 +29,49 @@ const ItemPage = () => {
     <Content>
       <Row justify="center" align="middle">
         <Col span={20}>
-          <Breadcrumb style={{ margin: "16px 0" }}>
+          <Breadcrumb className={styles.menuStyle}>
             <Breadcrumb.Item>Home</Breadcrumb.Item>
             <Breadcrumb.Item>List</Breadcrumb.Item>
             <Breadcrumb.Item>App</Breadcrumb.Item>
           </Breadcrumb>
         </Col>
       </Row>
-      <Row justify="center" align="top" style={{ height: "100vh" }}>
+      <Row justify="center" align="top">
         <Col span={20}>
           {itemsStatus === "loading" ? (
             <LoadingSpin />
           ) : (
             item && (
-              <div style={{ display: "flex", flexDirection: "column" }}>
-                <div style={{ display: "flex" }}>
-                  <Image src={item.picture} width={680} />
-                  <div>
-                    <h3
-                      style={{ fontSize: "14px" }}
-                    >{`${item.condition} - ${item.sold_quantity}`}</h3>
-                    <h1>{item.title}</h1>
-                    <h1>${item.price.amount}</h1>
+              <div className={styles.itemDetailContainer}>
+                <div className={styles.itemImgTitleContainer}>
+                  <img
+                    src={item.picture}
+                    className={styles.itemDetailImg}
+                    alt="product"
+                  />
+                  <div className={styles.itemDetailInfo}>
+                    <Title level={4} type="secondary" className={styles.sold}>
+                      {`${item.condition} - ${item.sold_quantity} vendidos`}
+                    </Title>
+                    <Title level={3} className={styles.title}>
+                      {item.title}
+                    </Title>
+                    <Title level={1} className={styles.price}>
+                      ${item.price.amount}
+                    </Title>
+                    <Button
+                      size="large"
+                      type="primary"
+                      icon={<ShoppingCartOutlined />}
+                      style={{ marginBottom: "32px" }}
+                    >
+                      Comprar
+                    </Button>
+                    <Title level={3}>Descripción del producto</Title>
+                    <Paragraph className={styles.description}>
+                      {item.description}
+                    </Paragraph>
                   </div>
-                </div>
-                <div>
-                  <h1>Descripción del producto</h1>
-                  <p>{item.description}</p>
                 </div>
               </div>
             )
