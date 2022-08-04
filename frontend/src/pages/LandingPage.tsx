f5bf02f020978cdf5d9ae9f14551e773b2e2444e
Daniel <danielrs9504@gmail.com>
2022-08-04 10:56:32 -0500

    Modifica landing de la pagina

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,19 +1,21 @@
 import React from "react";
-import { Col, Layout, Row, Typography } from "antd";
+import { Col, Layout, Result, Row } from "antd";
+import { SmileOutlined } from "@ant-design/icons";
 import styles from "../styles/App.module.css";
 
 const LandingPage = () => {
   // * ========== Variables ==========
   const { Content } = Layout;
-  const { Title } = Typography;
 
   return (
     <Content>
-      <Row justify="center" align="top" className={styles.content}>
+      <Row justify="center" align="middle" className={styles.content}>
         <Col span={20}>
-          <Title level={2}>
-            Usa el buscador para encontrar tu próxima compra!
-          </Title>
+          <Result
+            className={styles.welcome}
+            icon={<SmileOutlined />}
+            title="Bienvenido! Empieza a buscar tus productos"
+          />
         </Col>
       </Row>
     </Content>
